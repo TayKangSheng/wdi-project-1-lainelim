0aa5893224b7d8957bcc08b483ccf816b943f31d
elaine lim <lainex@gmail.com>
2017-02-02 11:34:22 +0800

    fixed mouseAppears on js2.js

--- assets/javascript/js2.js
+++ assets/javascript/js2.js
@@ -1,85 +1,102 @@
-function game(){
+var game = {
 
-  var mouses = document.querySelectorAll('.mouse')
-  var pows = document.querySelectorAll('.pow')
+  mouses: document.querySelectorAll('.mouse'),
+  pows: document.querySelectorAll('.pow'),
+  points: 0,
+  count: 0,
 
-  function getRandomInt (min, max) {
+  getRandomInt: function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min
+  },
+
+  start: function () {
+    var points = 0
+    var count = 0
+    scoreboard = document.querySelector('.scoreboard')
+    scoreboard.textContent = 'Mice Caught: 0'
+    game.mouses.forEach(function (mouse) {
+      mouse.classList.remove('visible')
+      mouse.classList.add('hidden')
+    })
+  },
+
+  getRandomMouse: function () {
+    this.i = Math.round(Math.random() * 11)
+    var mouse = this.mouses[this.i]
+    return mouse
+  },
+
+  mouseAppears: function () {
+    // var i = Math.round(Math.random() * 11)
+    // var mouse = game.mouses[i]
+    this.mouse = this.getRandomMouse()
+    console.log(this.mouse) // set this.mouse = to a random mouse
+    this.mouse.classList.remove('hidden')
+    this.mouse.classList.add('visible')
+    game.count++
+    setTimeout(this.mouseDisappears.bind(game), this.getRandomInt(500,2000))
+
+      // if(game.catchMouse()){
+      //   game.points++
+      //   game.updateScore()
+      // }
+      // else{
+      //
+      // }
+      // game.isGameOver()
+  },
+
+  catchMouse: function () {
+    this.mouse.onclick = function () {
+      this.pow = this.pows[this.i]
+      this.mouse.classList.remove('visible')
+      this.mouse.classList.add('hidden')
+      this.pow.classList.remove('hidden')
+      this.pow.classList.add('visible')
+      this.points++
+      setTimeout(function () {
+        this.pow.classList.remove('visible')
+        this.pow.classList.add('hidden')
+      }.bind(game), 500)
+    }.bind(game)
+  },
+
+  mouseDisappears: function () {
+    this.mouse.classList.remove('visible')
+    this.mouse.classList.add('hidden')
+  },
+
+  updateScore: function () {
+    scoreboard = document.querySelector('.scoreboard')
+    scoreboard.textContent = 'Mice Caught: ' + this.points
+  },
+
+  isGameOver: function () {
+    if (mousecount === 20) {
+      setTimeout(function () {
+        if (points >= 12) {
+          alert("You're a pro!")
+          clearInterval(this.start())
+        } else {
+          if (confirm('Would you like to try again?')) {
+            this.start()
+          } else {
+            clearInterval(this.start())
+          }
+        }
+      }, 3000)
+    }
   }
 
-function restart () {
-  points = 0
-  mousecount = 0
-  scoreboard = document.querySelector('.scoreboard')
-  scoreboard.textContent = 'Mice Caught: 0'
-  mouses.forEach(function (mouse) {
-    mouse.classList.remove('visible')
-    mouse.classList.add('hidden')
-  })
-}
-function start () {
-  setInterval(function(){
-  var i = Math.round(Math.random() * 11)
-  var mouse = mouses[i]
-  mouse.classList.remove('hidden')
-  mouse.classList.add('visible')
-  mousecount++
-  game.addClickHandler()
-  setTimeout(function () {
-    mouse.classList.remove('visible')
-    mouse.classList.add('hidden')
-  }, getRandomInt(500, 3000))
-  game.isGameOver()
-},2000)
-}
-
-function addClickHandler () {
-  mouse.onclick = function () {
-    var pow = pows[i]
-    mouse.classList.remove('visible')
-    mouse.classList.add('hidden')
-    pow.classList.remove('hidden')
-    pow.classList.add('visible')
-    points++
-    game.updateScore()
-    setTimeout(function () {
-      pow.classList.remove('visible')
-      pow.classList.add('hidden')
-    }, 500)
-  }
-}
-
-function updateScore () {
-  scoreboard = document.querySelector('.scoreboard')
-  scoreboard.textContent = 'Mice Caught: ' + points
-}
-
-function isGameOver () {
-  if (mousecount === 20) {
-    setTimeout(function () {
-      if (points >= 12) {
-        alert("You're a pro!")
-        clearInterval(game.start())
-      } else {
-        if (confirm('Would you like to try again?')) {
-          restart()
-        } else { clearInterval(game.start()) }
-      }
-    }, 3000)
-  }
-}
-return{
-start: start,
-restart: restart,
-isGameOver: isGameOver,
-updateScore: updateScore,
-addClickHandler: addClickHandler
-}
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  alert('Catch the mice by clicking on them as they appear!')
-  game.start()
-  var btn = document.querySelector('button')
-  btn.addEventListener('click', game.restart())
+  // alert('Catch the mice by clicking on them as they appear!')
+  // game.start()
+  setInterval(game.mouseAppears.bind(game), game.getRandomInt(500, 2000))
+  // game.mouseAppears()
+  // game.catchMouse()
+
+  // setTimeout(game.mouseDisappears.bind(game), game.getRandomInt(500,2000))
+
 })
