a6bc5d1c44f113a8ce67cefaa5b37d304cf3ca36
elaine lim <lainex@gmail.com>
2017-02-02 16:39:51 +0800

    deploy js2.js

--- assets/javascript/js2.js
+++ assets/javascript/js2.js
@@ -4,20 +4,23 @@ var game = {
   pows: document.querySelectorAll('.pow'),
   points: 0,
   count: 0,
+  scoreboard: document.querySelector('.scoreboard'),
+  gameEngine: null,
 
   getRandomInt: function (min, max) {
     return Math.floor(Math.random() * (max - min)) + min
   },
 
   start: function () {
-    var points = 0
-    var count = 0
-    scoreboard = document.querySelector('.scoreboard')
-    scoreboard.textContent = 'Mice Caught: 0'
-    game.mouses.forEach(function (mouse) {
+    this.points = 0
+    this.count = 0
+    this.scoreboard.textContent = 'Mice Caught: 0'
+    this.mouses.forEach(function (mouse) {
       mouse.classList.remove('visible')
       mouse.classList.add('hidden')
     })
+    document.querySelector('button').style.display = 'none'
+    this.gameEngine = setInterval(this.mouseAppears.bind(game), 2000)
   },
 
   getRandomMouse: function () {
@@ -27,23 +30,12 @@ var game = {
   },
 
   mouseAppears: function () {
-    // var i = Math.round(Math.random() * 11)
-    // var mouse = game.mouses[i]
     this.mouse = this.getRandomMouse()
-    console.log(this.mouse) // set this.mouse = to a random mouse
     this.mouse.classList.remove('hidden')
     this.mouse.classList.add('visible')
-    game.count++
-    setTimeout(this.mouseDisappears.bind(game), this.getRandomInt(500,2000))
-
-      // if(game.catchMouse()){
-      //   game.points++
-      //   game.updateScore()
-      // }
-      // else{
-      //
-      // }
-      // game.isGameOver()
+    console.log(++this.count)
+    this.catchMouse()
+    setTimeout(this.mouseDisappears.bind(game), this.getRandomInt(500, 2000))
   },
 
   catchMouse: function () {
@@ -54,6 +46,7 @@ var game = {
       this.pow.classList.remove('hidden')
       this.pow.classList.add('visible')
       this.points++
+      this.updateScore()
       setTimeout(function () {
         this.pow.classList.remove('visible')
         this.pow.classList.add('hidden')
@@ -64,39 +57,36 @@ var game = {
   mouseDisappears: function () {
     this.mouse.classList.remove('visible')
     this.mouse.classList.add('hidden')
+    setTimeout(this.isGameOver(), 3000)
   },
 
   updateScore: function () {
-    scoreboard = document.querySelector('.scoreboard')
-    scoreboard.textContent = 'Mice Caught: ' + this.points
+    this.scoreboard.textContent = 'Mice Caught: ' + this.points
   },
 
   isGameOver: function () {
-    if (mousecount === 20) {
-      setTimeout(function () {
-        if (points >= 12) {
-          alert("You're a pro!")
-          clearInterval(this.start())
+    if (this.count === 20) {
+      if (this.points >= 15) {
+        alert("You're a pro!")
+        clearInterval(this.gameEngine)
+        document.querySelector('button').style.display = 'inline-block'
+      } else {
+        if (confirm('Would you like to try again?')) {
+          clearInterval(this.gameEngine)
+          this.start()
         } else {
-          if (confirm('Would you like to try again?')) {
-            this.start()
-          } else {
-            clearInterval(this.start())
-          }
+          clearInterval(this.gameEngine)
+          document.querySelector('button').style.display = 'inline-block'
         }
-      }, 3000)
+      }
     }
   }
-
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  // alert('Catch the mice by clicking on them as they appear!')
-  // game.start()
-  setInterval(game.mouseAppears.bind(game), game.getRandomInt(500, 2000))
-  // game.mouseAppears()
-  // game.catchMouse()
-
-  // setTimeout(game.mouseDisappears.bind(game), game.getRandomInt(500,2000))
-
+  var btn = document.querySelector('button')
+  btn.addEventListener('click', game.start.bind(game))
+  alert('Catch the mice by clicking on them as they appear!')
+  btn.style.display = 'none'
+  game.start()
 })
